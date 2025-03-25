4c776501ad05266b3d9d297199a14a67679ac855
Ceyda <122219025+ceydabezek@users.noreply.github.com>
2025-03-25 11:40:45 +0100

    Update yelp.js

--- api/yelp.js
+++ api/yelp.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create({
     baseURL:'https://api.yelp.com/v3/businesses/',
     headers: {
-        Authorization: 'Bearer q5eF3fraj085Ofu-aSn3645MnxSnYr0UCzYHhxs2zI7rxrrxQBnAQ93-3bQvsycueYPoILHN1IN2mjROcRDddHs6XiEGZS5Ab4eNi_XY9oul43bS3BirGebivvA_ZXYx'
+        Authorization: '.
     },
 
 });
